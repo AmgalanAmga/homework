9fd915be0b94bd0f7bad71d842c334f740df1b02
unknown <amka.storm9812@gmail.com>
2023-03-02 21:11:39 +0800

    finished

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import notifee from '@notifee/react-native';
 import { launchImageLibrary } from "react-native-image-picker";
-import { TapGestureHandler } from "react-native-gesture-handler";
+import { TapGestureHandler, GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaView, Button, Image, Pressable, Text } from 'react-native';
 import Animated, { useSharedValue, useAnimatedGestureHandler, useAnimatedStyle, withTiming } from "react-native-reanimated";
 
@@ -41,33 +41,31 @@ function App() {
   }));
   const scaleUpEmoji = useAnimatedGestureHandler({
     onStart: () => {
-      emojiScaleup.value = withTiming(1);
+      emojiScaleup.value = withTiming(emojiScaleup.value);
     },
     onEnd: () => {
-      emojiScaleup.value = withTiming(2);
-    }
+      emojiScaleup.value = withTiming(emojiScaleup.value + 1);
+    },
   });
   return (
-    <SafeAreaView style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-      <Button title="Get Image" onPress={getImage} />
-      <Button title="Display notification" onPress={displayNotification} />
-      <Image source={{ uri: imgUrl ? imgUrl : defaultImg }} style={{ width: 300, height: 300, borderRadius: 10, marginTop: 25 }} />
-      <Pressable onPress={selectEmoji} style={{ height: 70, width: 70, backgroundColor: "#4adede", display: "flex", alignItems: "center", justifyContent: "center", borderRadius: 50, marginTop: 25 }}>
-        <Text style={{ fontSize: 50 }}>+</Text>
-      </Pressable>
-      {emoji &&
-        <TapGestureHandler
-          onGestureEvent={scaleUpEmoji}
-          numberOfTaps={1}
-          onActivated={() => {
-            console.log("res")
-          }}>
+    <GestureHandlerRootView>
+      <SafeAreaView style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <Button title="Get Image" onPress={getImage} />
+        <Button title="Display notification" onPress={displayNotification} />
+        <Image source={{ uri: imgUrl ? imgUrl : defaultImg }} style={{ width: 300, height: 300, borderRadius: 10, marginTop: 25 }} />
+        <Pressable onPress={selectEmoji} style={{ height: 70, width: 70, backgroundColor: "#4adede", display: "flex", alignItems: "center", justifyContent: "center", borderRadius: 50, marginTop: 25 }}>
+          <Text style={{ fontSize: 50 }}>+</Text>
+        </Pressable>
+        <TapGestureHandler onGestureEvent={scaleUpEmoji}
+          numberOfTaps={2}>
           <Animated.View style={[scalingStyle]}>
-            <Image source={{ uri: emoji }} style={{ width: 50, height: 50, borderRadius: 25, marginTop: 25 }} />
+            {emoji &&
+              <Image source={{ uri: emoji }} style={{ width: 50, height: 50, borderRadius: 25, marginTop: 25 }} />
+            }
           </Animated.View>
         </TapGestureHandler>
-      }
-    </SafeAreaView>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
